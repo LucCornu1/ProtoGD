fecc5c64b221cb85aa7d3590a43886aa8f687f7b
Luc Cornu <luccornu@hotmail.fr>
2022-06-08 11:13:01 +0200

    save

--- Scenes/Actors/CelestialObjects/CelestialObject.gd
+++ Scenes/Actors/CelestialObjects/CelestialObject.gd
@@ -5,37 +5,18 @@ func is_class(value: String): return value == "CelestialObject" or .is_class(val
 func get_class() -> String: return "CelestialObject"
 
 # Export variables
-export(float) var mass_Kg = 0.0
 
 # Onready variables
 
 # Variables
-var _speed : Vector2 = Vector2(0.0, 0.0) setget set_speed, get_speed
-var _acceleration : Vector2 = Vector2(0.0, 0.0) setget set_acceleration, get_acceleration
-var _applied_force : Vector2 = Vector2(0.0, -20.0) setget set_applied_force, get_applied_force
+var _applied_force : Vector2 = Vector2(0.0, 0.0) setget set_applied_force, get_applied_force
 
 
 #### ACCESSORS ####
-func set_speed(value : Vector2) -> void:
-	if value != _speed:
-		_speed = value
-		emit_signal("_speed_changed")
-
-func get_speed() -> Vector2:
-	return _speed
-
-func set_acceleration(value : Vector2) -> void:
-	if value != _acceleration:
-		_acceleration = value
-		emit_signal("_acceleration_changed")
-
-func get_acceleration() -> Vector2:
-	return _acceleration
-
 func set_applied_force(value : Vector2) -> void:
 	if value != _applied_force:
 		_applied_force = value
-		emit_signal("_applied_force_changed")
+		#emit_signal("_applied_force_changed")
 
 func get_applied_force() -> Vector2:
 	return _applied_force
---
+++ Scenes/Levels/DefaultLevel.gd
@@ -0,0 +1,58 @@
+extends Node2D
+class_name DefaultLevel
+
+func is_class(value: String): return value == "DefaultLevel" or .is_class(value)
+func get_class() -> String: return "DefaultLevel"
+
+# Export variables
+
+# Onready variables
+onready var _ysort : YSort = get_node("YSort")
+onready var _character : CelestialObject = get_node_or_null("CelestialObject3")
+
+# Variables
+
+
+#### ACCESSORS ####
+
+
+#### BUILT-IN ####
+func _ready() -> void:
+	pass
+
+func _physics_process(_delta : float) -> void:
+	_compute_forces()
+
+
+#### VIRTUALS ####
+
+
+#### LOGIC ####
+func _compute_forces() -> void:
+#	for _i in _ysort.get_children():
+#		if _i.is_class("CelestialObject"):
+#			_i.set_applied_force(Vector2(0.0, 0.0))
+#			for _j in _ysort.get_children():
+#				if _j.is_class("CelestialObject") and _i != _j:
+#					var grav = _compute_gravity(_i, _j)
+#					_i.set_applied_force(_i.get_applied_force() + grav)
+	
+	if _character.is_class("CelestialObject"):
+		_character.set_applied_force(Vector2(0.0, 0.0))
+		for _j in _ysort.get_children():
+			if _j.is_class("CelestialObject") and _character != _j:
+				var grav = _compute_gravity(_character, _j)
+				_character.set_applied_force(_character.get_applied_force() + grav)
+
+func _compute_gravity(A : CelestialObject, B : CelestialObject) -> Vector2:
+	return Vector2(0.0, 0.0)
+	# P = m*g
+	
+	# G * ((A.mass * B.mass) / A.position.distance_squared_to(B.position) / 100) * A.position.direction_to(B.position)
+	# G * (M_a * M_b / d²) * (B_Vector - A_Vector).normalized()
+
+
+#### INPUTS ####
+
+
+#### SIGNAL RESPONSES ####
